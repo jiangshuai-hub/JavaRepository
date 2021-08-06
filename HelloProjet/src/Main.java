3ce997e45fc313c0a1ab213121472215510a6fc0
jiangshuai-hub <57755732+jiangshuai-hub@users.noreply.github.com>
2021-08-06 10:30:26 +0800

    Update Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,11 +5,12 @@ public class Main {
 
     @Test
     public void test() {
-
-        System.out.println("Hello Github!");
-        System.out.println("Hello World!");
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             System.out.println(i);
         }
+        System.out.println("Hello Github!");
+        System.out.println("===============");
+        System.out.println("Hello World!");
+        
     }
 }
