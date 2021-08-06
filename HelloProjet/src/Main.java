3193b086d33c98f3037790f35af9a2d89cbaacd0
jiangshuai <jiangshuai05@kuaishou.com>
2021-08-06 10:26:40 +0800

    修改了HelloProject项目文件

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,10 +1,15 @@
 import org.junit.Test;
 
+
 public class Main {
 
     @Test
-    public void testHelloWrold() {
-       
-		 System.out.println("Hello Github!");
+    public void test() {
+
+        System.out.println("Hello Github!");
+        System.out.println("Hello World!");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 }
